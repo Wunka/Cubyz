e40a52000484a3ddc2b194852e117493194b4618
Wunka <56411012+Wunka@users.noreply.github.com>
2026-03-10 20:31:17 +0100

    bring save_creation to old state

--- src/gui/windows/save_creation.zig
+++ src/gui/windows/save_creation.zig
@@ -7,7 +7,6 @@ const ConnectionManager = main.network.ConnectionManager;
 const settings = main.settings;
 const Vec2f = main.vec.Vec2f;
 const NeverFailingAllocator = main.heap.NeverFailingAllocator;
-const Texture = main.graphics.Texture;
 const ZonElement = main.ZonElement;
 
 const gui = @import("../gui.zig");
@@ -19,7 +18,6 @@ const Label = @import("../components/Label.zig");
 const TextInput = @import("../components/TextInput.zig");
 const CheckBox = @import("../components/CheckBox.zig");
 const VerticalList = @import("../components/VerticalList.zig");
-const TabList = @import("../components/TabList.zig");
 
 pub var window = GuiWindow{
 	.contentSize = Vec2f{128, 256},
@@ -39,66 +37,6 @@ var worldPresets: []ZonMapEntry = &.{};
 var selectedPreset: usize = undefined;
 var defaultPreset: usize = 0;
 var presetButton: *Button = undefined;
-
-var needsUpdate: bool = false;
-
-var deleteIcon: Texture = undefined;
-var fileExplorerIcon: Texture = undefined;
-
-var tabs: *TabList = undefined;
-var tabName: *Label = undefined;
-
-fn getGenerationTab() *VerticalList {
-	const submenu = VerticalList.init(.{0, 0}, 384, 8);
-	const maxWidth = 192;
-	{ // world preset
-		const row = HorizontalList.init();
-		row.add(Label.init(.{0, 0}, maxWidth - 128, "Preset:", .left));
-		presetButton = Button.initText(.{0, 0}, 128, worldPresets[selectedPreset].key_ptr.*, .init(worldPresetCallback));
-		row.add(presetButton);
-		row.finish(.{0, 0}, .center);
-		submenu.add(row);
-	}
-
-	{ // seed
-		const row = HorizontalList.init();
-		row.add(Label.init(.{0, 0}, 48, "Seed:", .left));
-		seedInput = TextInput.init(.{0, 0}, maxWidth - 48, 22, "", .{.onNewline = .init(createWorld)});
-		row.add(seedInput);
-		row.finish(.{0, 0}, .center);
-		submenu.add(row);
-	}
-	submenu.finish(.center);
-	return submenu;
-}
-fn getGameruleTab() *VerticalList {
-	const submenu = VerticalList.init(.{0, 0}, 384, 8);
-	const maxWidth = 192;
-	{
-		const row = HorizontalList.init();
-		row.add(Label.init(.{0, 0}, maxWidth - 96, "Game Mode:", .left));
-		gamemodeInput = Button.initText(.{0, 0}, 96, @tagName(worldSettings.defaultGamemode), .init(gamemodeCallback));
-		row.add(gamemodeInput);
-		row.finish(.{0, 0}, .center);
-		submenu.add(row);
-	}
-
-	submenu.add(CheckBox.init(.{0, 0}, maxWidth, "Allow Cheats", worldSettings.allowCheats, &allowCheatsCallback));
-	submenu.finish(.center);
-	return submenu;
-}
-
-fn prevPage() void {
-	tabs.previousTab();
-	tabName.updateText(tabs.getTitle());
-	needsUpdate = true;
-}
-
-fn nextPage() void {
-	tabs.nextTab();
-	tabName.updateText(tabs.getTitle());
-	needsUpdate = true;
-}
 
 fn chooseSeed(seedStr: []const u8) u64 {
 	if (seedStr.len == 0) {
@@ -142,7 +80,7 @@ fn createWorld() void {
 }
 
 pub fn onOpen() void {
-	const list = VerticalList.init(.{padding, 16 + padding}, 500, 8);
+	const list = VerticalList.init(.{padding, 16 + padding}, 300, 8);
 
 	if (worldPresets.len == 0) {
 		var presetMap = main.assets.worldPresets();
@@ -164,48 +102,39 @@ pub fn onOpen() void {
 			}
 		}
 	}
-	if (!needsUpdate) selectedPreset = defaultPreset;
-	tabs = TabList.init(.{0, 8});
-	tabs.add("Generation (1/2)", getGenerationTab());
-	tabs.add("Game Rules (2/2)", getGameruleTab());
-	tabs.finish();
+	selectedPreset = defaultPreset;
 
-	{ // name field
-		const label = Label.init(.{0, 0}, 96, "World Name:", .center);
-		var num: usize = 1;
-		while (true) {
-			const path = std.fmt.allocPrint(main.stackAllocator.allocator, "saves/Save{}", .{num}) catch unreachable;
-			defer main.stackAllocator.free(path);
-			if (!main.files.cubyzDir().hasDir(path)) break;
-			num += 1;
-		}
-		const name = std.fmt.allocPrint(main.stackAllocator.allocator, "Save{}", .{num}) catch unreachable;
-		defer main.stackAllocator.free(name);
-		nameInput = TextInput.init(.{0, 0}, 256 - 96, 22, name, .{.onNewline = .init(createWorld)});
-		const nameRow = HorizontalList.init();
-		nameRow.add(label);
-		nameRow.add(nameInput);
-		nameRow.finish(.{0, 0}, .center);
-		list.add(nameRow);
+	var num: usize = 1;
+	while (true) {
+		const path = std.fmt.allocPrint(main.stackAllocator.allocator, "saves/Save{}", .{num}) catch unreachable;
+		defer main.stackAllocator.free(path);
+		if (!main.files.cubyzDir().hasDir(path)) break;
+		num += 1;
 	}
+	const name = std.fmt.allocPrint(main.stackAllocator.allocator, "Save{}", .{num}) catch unreachable;
+	defer main.stackAllocator.free(name);
+	nameInput = TextInput.init(.{0, 0}, 128, 22, name, .{.onNewline = .init(createWorld)});
+	list.add(nameInput);
 
-	{ // page title and switch buttons
-		const leftArrow = Button.initText(.{0, 0}, 24, "<", .init(prevPage));
-		tabName = Label.init(.{0, 0}, 224 - 48, tabs.getTitle(), .center);
-		const rightArrow = Button.initText(.{0, 0}, 24, ">", .init(nextPage));
-		const header = HorizontalList.init();
-		header.add(leftArrow);
-		header.add(tabName);
-		header.add(rightArrow);
-		header.finish(.{0, 0}, .center);
-		list.add(header);
-	}
+	gamemodeInput = Button.initText(.{0, 0}, 128, @tagName(worldSettings.defaultGamemode), .init(gamemodeCallback));
+	list.add(gamemodeInput);
 
-	list.add(tabs);
+	list.add(CheckBox.init(.{0, 0}, 128, "Allow Cheats", worldSettings.allowCheats, &allowCheatsCallback));
 
 	if (!build_options.isTaggedRelease) {
-		list.add(CheckBox.init(.{0, 0}, 192, "Testing mode (for developers)", worldSettings.testingMode, &testingModeCallback));
+		list.add(CheckBox.init(.{0, 0}, 128, "Testing mode (for developers)", worldSettings.testingMode, &testingModeCallback));
 	}
+
+	presetButton = Button.initText(.{0, 0}, 128, worldPresets[selectedPreset].key_ptr.*, .init(worldPresetCallback));
+	list.add(presetButton);
+
+	const seedLabel = Label.init(.{0, 0}, 48, "Seed:", .left);
+	seedInput = TextInput.init(.{0, 0}, 128 - 48, 22, "", .{.onNewline = .init(createWorld)});
+	const seedRow = HorizontalList.init();
+	seedRow.add(seedLabel);
+	seedRow.add(seedInput);
+	seedRow.finish(.{0, 0}, .center);
+	list.add(seedRow);
 
 	list.add(Button.initText(.{0, 0}, 128, "Create World", .init(createWorld)));
 
