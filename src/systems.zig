b7eb24e57755308c903a040b228fea9e4ae36ae7
Wunka <56411012+Wunka@users.noreply.github.com>
2026-08-04 20:14:01 +0200

    fix merge

--- src/systems.zig
+++ src/systems.zig
@@ -9,47 +9,47 @@ pub const systems = @import("systems/_list.zig");
 
 pub const client = struct {
 	pub fn init() void {
-		inline for (@typeInfo(systems).@"struct".decls) |decl| {
-			@field(systems, decl.name).client.init();
+		inline for (@typeInfo(systems).@"struct".decl_names) |declName| {
+			@field(systems, declName).client.init();
 		}
 	}
 	pub fn deinit() void {
-		inline for (@typeInfo(systems).@"struct".decls) |decl| {
-			@field(systems, decl.name).client.deinit();
+		inline for (@typeInfo(systems).@"struct".decl_names) |declName| {
+			@field(systems, declName).client.deinit();
 		}
 	}
 	pub fn clear() void {
-		inline for (@typeInfo(systems).@"struct".decls) |decl| {
-			@field(systems, decl.name).client.clear();
+		inline for (@typeInfo(systems).@"struct".decl_names) |declName| {
+			@field(systems, declName).client.clear();
 		}
 	}
 	pub fn render(ambientLight: Vec3f, playerPos: Vec3d, deltaTime: f64) void {
 		main.client.entity_manager.update();
-		inline for (@typeInfo(systems).@"struct".decls) |decl| {
-			@field(systems, decl.name).client.render(ambientLight, playerPos, deltaTime);
+		inline for (@typeInfo(systems).@"struct".decl_names) |declName| {
+			@field(systems, declName).client.render(ambientLight, playerPos, deltaTime);
 		}
 	}
 	pub fn renderHud(ambientLight: Vec3f, playerPos: Vec3d) void {
-		inline for (@typeInfo(systems).@"struct".decls) |decl| {
-			@field(systems, decl.name).client.renderHud(ambientLight, playerPos);
+		inline for (@typeInfo(systems).@"struct".decl_names) |declName| {
+			@field(systems, declName).client.renderHud(ambientLight, playerPos);
 		}
 	}
 };
 
 pub const server = struct {
 	pub fn init() void {
-		inline for (@typeInfo(systems).@"struct".decls) |decl| {
-			@field(systems, decl.name).server.init();
+		inline for (@typeInfo(systems).@"struct".decl_names) |declName| {
+			@field(systems, declName).server.init();
 		}
 	}
 	pub fn deinit() void {
-		inline for (@typeInfo(systems).@"struct".decls) |decl| {
-			@field(systems, decl.name).server.deinit();
+		inline for (@typeInfo(systems).@"struct".decl_names) |declName| {
+			@field(systems, declName).server.deinit();
 		}
 	}
 	pub fn update() void {
-		inline for (@typeInfo(systems).@"struct".decls) |decl| {
-			@field(systems, decl.name).server.update();
+		inline for (@typeInfo(systems).@"struct".decl_names) |declName| {
+			@field(systems, declName).server.update();
 		}
 	}
 };
